832d9f6e70cf8b7133f09f3b6c2cf7881548b2e0
rat-moonshine <devsena@yahoo.com>
2021-03-11 14:38:30 +0530

    First commit of Haxe library

---
+++ MoonshineSDKInstallerGUICore/bin/flash/haxe/ManifestResources.hx
@@ -0,0 +1,123 @@
+package;
+
+
+import haxe.io.Bytes;
+import lime.utils.AssetBundle;
+import lime.utils.AssetLibrary;
+import lime.utils.AssetManifest;
+import lime.utils.Assets;
+
+#if sys
+import sys.FileSystem;
+#end
+
+@:access(lime.utils.Assets)
+
+
+@:keep @:dox(hide) class ManifestResources {
+
+
+	public static var preloadLibraries:Array<AssetLibrary>;
+	public static var preloadLibraryNames:Array<String>;
+	public static var rootPath:String;
+
+
+	public static function init (config:Dynamic):Void {
+
+		preloadLibraries = new Array ();
+		preloadLibraryNames = new Array ();
+
+		rootPath = null;
+
+		if (config != null && Reflect.hasField (config, "rootPath")) {
+
+			rootPath = Reflect.field (config, "rootPath");
+
+		}
+
+		if (rootPath == null) {
+
+			#if (ios || tvos || emscripten)
+			rootPath = "assets/";
+			#elseif android
+			rootPath = "";
+			#elseif console
+			rootPath = lime.system.System.applicationDirectory;
+			#else
+			rootPath = "./";
+			#end
+
+		}
+
+		#if (openfl && !flash && !display)
+		
+		#end
+
+		var data, manifest, library, bundle;
+
+		#if kha
+
+		null
+		library = AssetLibrary.fromManifest (manifest);
+		Assets.registerLibrary ("null", library);
+
+		if (library != null) preloadLibraries.push (library);
+		else preloadLibraryNames.push ("null");
+
+		#else
+
+		data = '{"name":null,"assets":"ah","rootPath":null,"version":2,"libraryArgs":[],"libraryType":null}';
+		manifest = AssetManifest.parse (data, rootPath);
+		library = AssetLibrary.fromManifest (manifest);
+		Assets.registerLibrary ("default", library);
+		
+
+		library = Assets.getLibrary ("default");
+		if (library != null) preloadLibraries.push (library);
+		else preloadLibraryNames.push ("default");
+		
+
+		#end
+
+	}
+
+
+}
+
+
+#if kha
+
+null
+
+#else
+
+#if !display
+#if flash
+
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__manifest_default_json extends flash.utils.ByteArray { }
+
+
+#elseif (desktop || cpp)
+
+@:keep @:file("") @:noCompletion #if display private #end class __ASSET__manifest_default_json extends haxe.io.Bytes {}
+
+
+
+#else
+
+
+
+#end
+
+#if (openfl && !flash)
+
+#if html5
+
+#else
+
+#end
+
+#end
+#end
+
+#end
