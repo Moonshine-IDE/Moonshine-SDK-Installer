6b70cdc02c98f5d309de38db0fc968e0596f56cf
devsena <devsena@yahoo.com>
2021-06-16 18:25:58 +0530

    Adding untested changes on replacing tasks to Harman install/download-script (#56)

--- MoonshineSDKInstallerGUICore/bin/flash/haxe/ApplicationMain.hx
+++ MoonshineSDKInstallerGUICore/bin/flash/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "293";
+		app.meta["build"] = "302";
 		app.meta["company"] = "Prominic.NET";
 		app.meta["file"] = "MoonshineSDKInstallerGUICore";
 		app.meta["name"] = "MoonshineSDKInstallerGUICore";
