a0e93f3586d42d36911392725aee2c9bca6e842b
devsena <devsena@yahoo.com>
2021-07-01 15:35:28 +0530

    - Font updates - Restoring one image file (prominic/Moonshine-IDE#863)

--- MoonshineSDKInstallerGUICore/bin/flash/haxe/ApplicationMain.hx
+++ MoonshineSDKInstallerGUICore/bin/flash/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "313";
+		app.meta["build"] = "317";
 		app.meta["company"] = "Prominic.NET";
 		app.meta["file"] = "MoonshineSDKInstallerGUICore";
 		app.meta["name"] = "MoonshineSDKInstallerGUICore";
